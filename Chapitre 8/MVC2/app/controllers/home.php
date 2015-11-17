1d6f0ddf3b47a45eb1c933d3efc146ddf1ca5898
unknown <michel.di.croci@J-C222-OL-00.CRL.int>
2015-11-17 10:52:48 -0500

    MVC2 - Nouvelle Version

--- Chapitre 8/MVC2/app/controllers/home.php
+++ Chapitre 8/MVC2/app/controllers/home.php
@@ -4,10 +4,16 @@ class Home extends Controller
 {
 	public function index( $name = '')
 	{
-		$user = $this -> model ('User');
-		$user->name = $name;
-
-		$this->view['home/index'], ['name' => $user->name]);
+		//echo "allo " . $name;
+		//$user = $this -> model ('User');
+		//$user->name = $name;
+		parent::view('home/index', ['name' => $name]);
 	}
+
+	public function patate ()
+	{
+		echo "J'aime les patates";
+	}
+	
 
 }
--- Chapitre 8/MVC2/app/core/App.php
+++ Chapitre 8/MVC2/app/core/App.php
@@ -13,10 +13,10 @@ class App
 		if (file_exists('../app/controllers/' . $url[0] . '.php'))
 		{
 			$this->controller = $url[0];
-			unser ($url[0]);
+			unset($url[0]);
 		}
 
-		require_once '../app/controllers/' . $this->controller . 'php';
+		require_once '../app/controllers/' . $this->controller . '.php';
 
 		if (isset($url[1]))
 		{
@@ -29,15 +29,25 @@ class App
 
 		$this->params = $url ? array_values ($url) : [] ;
 
-		call_user_func_array([ $this->controller, $this->method, $this->params]);
+		call_user_func_array([$this->controller, $this->method], $this->params );
 	}
 
 	public function parseUrl()
 	{
 
-		if (isset($_GET['URL'])) {
-			return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
-		}
+		$url = parse_url($_SERVER["REQUEST_URI"])["path"];
+		$url = array_filter(explode("/", $url));
+		$pos = array_search("index.php", $url);
+
+		for ($i = 0; $i <= $pos; $i++)
+			unset($url[$i]);
+
+		return $url = array_values ($url);
+
+		//print_r($_GET['URL']);
+		//if (isset($_GET['URL'])) {
+		//	return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+		
 	}
 
 }
